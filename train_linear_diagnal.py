787e8200a3933e82b127f9ff72a333862a953378
Yi Zhou <33936123+Jodiechou@users.noreply.github.com>
2020-05-30 20:09:28 +0100

    Update train_linear_diagnal.py

--- train_linear_diagnal.py
+++ train_linear_diagnal.py
@@ -1,18 +1,15 @@
 import numpy as np 
 import torch.optim as optim
 import torch
-from torch.autograd import Variable
 
 import logging
 import argparse
-
 import lxml.etree
-import xml.etree.ElementTree as ET
+import os
+from transformers import BertTokenizer, BertModel, BertForMaskedLM
 
-from bert_as_service import tokenizer as bert_tokenizer
-from bert_as_service import bert_embed
 
-torch.cuda.set_device(1)
+os.environ['CUDA_VISIBLE_DEVICES'] = '1'
 
 logging.basicConfig(level=logging.DEBUG,
 					format='%(asctime)s - %(levelname)s - %(message)s',
@@ -22,37 +19,40 @@ def load_training_set(train_path, keys_path):
 	"""Parse XML of split set and return list of instances (dict)."""
 	train_instances = []
 	sense_mapping = get_sense_mapping(keys_path)
-	tree = ET.parse(train_path)
-	for text in tree.getroot():
-		for sent_idx, sentence in enumerate(text):
-			inst = {'tokens': [], 'tokens_mw': [], 'lemmas': [], 'senses': [], 'pos': [], 'id': []}
-			for e in sentence:
-				inst['tokens_mw'].append(e.text)
-				inst['lemmas'].append(e.get('lemma'))
-				inst['id'].append(e.get('id'))
-				inst['pos'].append(e.get('pos'))
-				if 'id' in e.attrib.keys():
-					inst['senses'].append(sense_mapping[e.get('id')])
-				else:
-					inst['senses'].append(None)
+	# tree = ET.parse(train_path)
+	# for text in tree.getroot():
+	text = read_xml_sents(train_path)
+	for sent_idx, sentence in enumerate(text):
+		inst = {'tokens': [], 'tokens_mw': [], 'lemmas': [], 'senses': [], 'pos': [], 'id': []}
+		for e in sentence:
+			inst['tokens_mw'].append(e.text)
+			inst['lemmas'].append(e.get('lemma'))
+			inst['id'].append(e.get('id'))
+			inst['pos'].append(e.get('pos'))
+			if 'id' in e.attrib.keys():
+				inst['senses'].append(sense_mapping[e.get('id')])
+			else:
+				inst['senses'].append(None)
 
-			inst['tokens'] = sum([t.split() for t in inst['tokens_mw']], [])
+		inst['tokens'] = sum([t.split() for t in inst['tokens_mw']], [])
 
-			# handling multi-word expressions, mapping allows matching tokens with mw features
-			idx_map_abs = []
-			idx_map_rel = [(i, list(range(len(t.split()))))
-							for i, t in enumerate(inst['tokens_mw'])]
-			token_counter = 0
-			for idx_group, idx_tokens in idx_map_rel:  # converting relative token positions to absolute
-				idx_tokens = [i+token_counter for i in idx_tokens]
-				token_counter += len(idx_tokens)
-				idx_map_abs.append([idx_group, idx_tokens])
-			inst['tokenized_sentence'] = ' '.join(inst['tokens'])
-			inst['idx_map_abs'] = idx_map_abs
-			inst['idx'] = sent_idx
-			train_instances.append(inst)
+		"""handling multi-word expressions, mapping allows matching tokens with mw features"""
+		idx_map_abs = []
+		idx_map_rel = [(i, list(range(len(t.split()))))
+						for i, t in enumerate(inst['tokens_mw'])]
+		token_counter = 0
+		"""converting relative token positions to absolute"""
+		for idx_group, idx_tokens in idx_map_rel:  
+			idx_tokens = [i+token_counter for i in idx_tokens]
+			token_counter += len(idx_tokens)
+			idx_map_abs.append([idx_group, idx_tokens])
+		inst['tokenized_sentence'] = ' '.join(inst['tokens'])
+		inst['idx_map_abs'] = idx_map_abs
+		inst['idx'] = sent_idx
+		train_instances.append(inst)
 
 	return train_instances
+
 
 def chunks(l, n):
 	"""Yield successive n-sized chunks from given list."""
@@ -98,17 +98,22 @@ def save_pickle_dict(path, mat):
 
 
 def get_args(
-		num_epochs = 5,
-		emb_dim = 300,
-		diag = False
+		num_epochs = 50,
+		##emb_dim = 300,
+		##emb_dim = 768,
+		emb_dim = 1024,
+		batch_size = 64,
+		diag = False,
+		lr = 1e-3
 			 ):
 
-	parser = argparse.ArgumentParser(description='BERT Word Sense Embeddings')
-	parser.add_argument('--glove_embedding_path', default='external/glove/glove.840B.300d.txt')
-	parser.add_argument('--sense_matrix_path', type=str, default='data/vectors/senseMatrix.semcor_{}_{}.txt'.format(emb_dim, emb_dim))
-	parser.add_argument('--save_sense_emb_path', default='data/vectors/senseEmbed.semcor_{}.txt'.format(emb_dim))
-	parser.add_argument('--save_sense_matrix_path', default='data/vectors/senseEmbed.semcor_{}.npz'.format(emb_dim))
-	parser.add_argument('--save_weight_path', default='data/vectors/weight.semcor_1024_{}.npz'.format(emb_dim))
+	parser = argparse.ArgumentParser(description='Word Sense Mapping')
+	##parser.add_argument('--glove_embedding_path', default='external/glove/glove.768.txt')
+	parser.add_argument('--glove_embedding_path', default='external/glove/glove.1024.txt')
+	##parser.add_argument('--glove_embedding_path', default='external/glove/glove.840B.300d.txt')
+	# parser.add_argument('--sense_matrix_path', type=str, default='data/vectors/senseMatrix.semcor_diagonalI_{}_{}_5.txt'.format(emb_dim, emb_dim))
+	parser.add_argument('--save_sense_matrix_path', default='data/vectors/senseMatrix.semcor_diagonal_{}_50.npz'.format(emb_dim))
+	parser.add_argument('--save_weight_path', default='data/vectors/weight.semcor_diagonal_1024_{}_50.npz'.format(emb_dim))
 	parser.add_argument('--num_epochs', default=num_epochs, type=int)
 	parser.add_argument('--loss', default='standard', type=str, choices=['standard'])
 	parser.add_argument('--emb_dim', default=emb_dim, type=int)
@@ -118,10 +123,11 @@ def get_args(
 						default='external/wsd_eval/WSD_Evaluation_Framework/')
 	parser.add_argument('--dataset', default='semcor', help='Name of dataset', required=False,
 						choices=['semcor', 'semcor_omsti'])
-	parser.add_argument('--batch_size', type=int, default=32, help='Batch size (BERT)', required=False)
+	parser.add_argument('--batch_size', type=int, default=batch_size, help='Batch size', required=False)
+	parser.add_argument('--lr', type=float, default=lr, help='Learning rate', required=False)
 	parser.add_argument('--merge_strategy', type=str, default='mean', help='WordPiece Reconstruction Strategy', required=False,
 						choices=['mean', 'first', 'sum'])
-
+		
 	args = parser.parse_args()
 
 	return args
@@ -137,6 +143,49 @@ def load_glove_embeddings(fn):
 			vec = np.array(splitLine[1:], dtype='float32')
 			embeddings[word] = vec
 	return embeddings
+
+
+def get_bert_embedding(sent):
+	"""
+	input: a sentence
+	output: word embeddigns for the words apprearing in the sentence
+	"""
+	tokenized_text = tokenizer.tokenize("[CLS] {0} [SEP]".format(sent))
+	indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
+	segments_ids = [0 for i in range(len(indexed_tokens))]
+	tokens_tensor = torch.tensor([indexed_tokens])
+	segments_tensors = torch.tensor([segments_ids])
+	tokens_tensor = tokens_tensor.to(device)
+	segments_tensors = segments_tensors.to(device)
+	model.to(device)
+	with torch.no_grad():
+		outputs = model(tokens_tensor, token_type_ids=segments_tensors)
+	"""[1:-1] is used to get rid of CLS] and [SEP]"""
+	res = list(zip(tokenized_text[1:-1], outputs[0].cpu().detach().numpy()[0][1:-1])) 
+	
+	"""merge subtokens"""
+	sent_tokens_vecs = []
+	for token in sent.split():
+		token_vecs = []
+		sub = []
+		for subtoken in tokenizer.tokenize(token):
+			encoded_token, encoded_vec = res.pop(0)
+			sub.append(encoded_token)
+			token_vecs.append(encoded_vec)
+			merged_vec = np.array(token_vecs, dtype='float32').mean(axis=0) 
+			merged_vec = torch.from_numpy(merged_vec.reshape(1024, 1)).to(device)
+			##merged_vec = torch.from_numpy(merged_vec).to(device)
+		sent_tokens_vecs.append((token, merged_vec))
+	return sent_tokens_vecs
+
+
+def adjust_learning_rate(optimizer, epoch):
+	"""Sets the learning rate to the initial LR decayed by 10 every 30 epochs"""
+	lr = args.lr * (0.1 ** (epoch // 30))
+	for param_group in optimizer.param_groups:
+		param_group['lr'] = lr
+
+
 
 if __name__ == '__main__':
 
@@ -155,20 +204,29 @@ if __name__ == '__main__':
 
 	device = torch.device(args.device)
 
-	sense2idx, sense2matrix, sense_matrix = {}, {}, {}
+	sense2idx, sense_matrix = {}, {}
 	idx, index, out_of_vocab_num = 0, 0, 0
 
-	lr = 1e-3
 	
-	logging.info("Loading Glove Embeddings........")
-	glove_embeddings = load_glove_embeddings(args.glove_embedding_path)
-	logging.info("Done. Loaded %d words from GloVe embeddings" % len(glove_embeddings))
+	tokenizer = BertTokenizer.from_pretrained('bert-large-cased')
+	model = BertModel.from_pretrained('bert-large-cased')
+	##tokenizer = BertTokenizer.from_pretrained('bert-base-cased')
+	##model = BertModel.from_pretrained('bert-base-cased')
+
+	model.eval()
 	
 	logging.info("Loading Training Data........")
 	train_instances = load_training_set(train_path, keys_path)
 	logging.info("Done. Loaded %d instances from dataset" % len(train_instances))
-	
-	# Build sense2idx dictionary
+
+	"""
+	build sense2idx dictionary
+	use dictionary to filter sense with the same id
+	"""
+	logging.info("Loading Glove Embeddings........")
+	glove_embeddings_full = load_glove_embeddings(args.glove_embedding_path)
+
+	glove_embeddings = {}
 	for sent_instance in train_instances:
 		for i in range(len(sent_instance['senses'])):
 			if sent_instance['senses'][i] is None:
@@ -180,36 +238,59 @@ if __name__ == '__main__':
 					continue
 
 				word = sense.split('%')[0]
-				if word not in glove_embeddings.keys():
+				if word not in glove_embeddings_full.keys():
 					out_of_vocab_num += 1
 					continue
 
+				glove_embeddings[word] = torch.from_numpy(glove_embeddings_full[word]).to(device)
 				sense2idx[sense] = idx
 				idx += 1
 
+	logging.info("Done. Loaded %d words from GloVe embeddings" % len(glove_embeddings))
 	num_senses = len(sense2idx)
-	print('num_senses', num_senses)
-	
-	# A = torch.randn(num_senses, args.emb_dim, args.emb_dim, requires_grad=True, dtype=torch.float32, device='cuda')
-	A = torch.randn(num_senses, args.emb_dim, dtype=torch.float32, device='cuda')
-	A = Variable(torch.diag_embed(A), requires_grad=True)
-	W = torch.randn(args.emb_dim, 1024, requires_grad=True, dtype=torch.float32, device='cuda')
-	optimizer = optim.Adam((A, W), lr)
+
+	A = []
+	for i in range(0, num_senses):
+		A.append(torch.randn(args.emb_dim, dtype=torch.float32, device=device, requires_grad=False))
+
+	##W = [torch.randn(args.emb_dim, 1024, dtype=torch.float32, device=device, requires_grad=True)]
+
+	##optimizer = optim.Adam(W+A, lr=args.lr)
+	optimizer = optim.Adam(A, lr=args.lr)
+
+	##vector_ones = torch.ones(args.emb_dim, dtype=torch.float32, device=device, requires_grad=False)
+
 	
 
 	logging.info("------------------Training-------------------")
 
 	for epoch in range(args.num_epochs):
+		cum_loss = 0  
+		count = 0
+		adjust_learning_rate(optimizer,epoch)
+
 		for batch_idx, batch in enumerate(chunks(train_instances, args.batch_size)):
-		
-			loss = 0
-			batch_sents = [sent_info['tokenized_sentence'] for sent_info in batch]
 
-			# process contextual embeddings in sentences batches of size args.batch_size
-			batch_bert = bert_embed(batch_sents, merge_strategy=args.merge_strategy)
+			"""
+			set all of the A matrices to requires_grad = False
+			optimizer.param_groups is a list which contains one dictionary
+			optimizer.param_groups[0]['params'] returns a list of trainable parameters 
+			""" 
 
-			for sent_info, sent_bert in zip(batch, batch_bert):
+			# for param_group in optimizer.param_groups[0]['params'][1:]:
+			# 	param_group.requires_grad = False
+			for a in A:
+				a.requires_grad = False
+
+			optimizer.zero_grad()
+			loss = torch.zeros(1, dtype=torch.float32).to(device)
+			count += 1
+
+			"""process contextual embeddings in sentences batches of size args.batch_size"""
+			for sent_info in batch:
 				idx_map_abs = sent_info['idx_map_abs']
+
+				sent_bert = get_bert_embedding(sent_info['tokenized_sentence'])
 
 				for mw_idx, tok_idxs in idx_map_abs:
 					if sent_info['senses'][mw_idx] is None:
@@ -221,43 +302,52 @@ if __name__ == '__main__':
 
 						index = sense2idx[sense]
 						word = sense.split('%')[0]
-
 						vec_g = glove_embeddings[word]
-						
-						# For the case of taking multiple words as a instance
-						# for example, obtaining the embedding for 'too much' instead of two embeddings for 'too' and 'much'
-						# we use mean to compute the averaged vec for a multiple words expression
-						vec_c = np.array([sent_bert[i][1] for i in tok_idxs], dtype=np.float32).mean(axis=0)
 
-						vec_g = torch.from_numpy(vec_g).view(300, 1).to(device)
-						vec_c = torch.from_numpy(vec_c).view(1024, 1).to(device)
+						"""
+						for the case of taking multiple words as a instance
+						for example, obtaining the embedding for 'too much' instead of two embeddings for 'too' and 'much'
+						we use mean to compute the averaged vec for a multiple words expression
+						"""
+						vec_c = torch.mean(torch.stack([sent_bert[i][1] for i in tok_idxs]), dim=0)
+
+						##loss += ((torch.mm(W[0], vec_c).squeeze(1)) - A[index] * vec_g).norm() ** 2
+						loss += (vec_c - A[index] * vec_g).norm() ** 2  ### no weights
+						##loss += (vec_c - A[index] * vec_g).norm() ** 2 + (A[index] - vector_ones).norm() ** 2  ### no weights with regulariser
+
+						"""set the A matrices that are in a current batch to require gradient"""
+						# optimizer.param_groups[0]['params'][1+index].requires_grad = True
+						A[index].requires_grad = True
 						
-						loss += (torch.mm(W, vec_c) - torch.mm(A[index], vec_g)).norm() ** 2
-						
-			optimizer.zero_grad()
+
+			cum_loss += loss.item()
 			loss.backward()
+
 			optimizer.step()
 
-		logging.info("epoch: %d, loss: %f " %(epoch, loss.item()))
+		cum_loss /= count
+		logging.info("epoch: %d, loss: %f " %(epoch, cum_loss))
 
-	weight = W.cpu().detach().numpy()
-	matrix_A = A.cpu().detach().numpy()
+
+	"""save the trained parameters W and A matrices"""
+	##weight = W[0].cpu().detach().numpy()
+	matrix_A = [A[i].cpu().detach().numpy() for i in range(num_senses)]
+
 
 	logging.info('number of out of vocab word: %d' %(out_of_vocab_num))
-	print('shape of each matrix A', matrix_A[0].shape)
-	print('shape of weight matrix w', weight.shape)	
+	print('shape of each matrix A:', matrix_A[0].shape)
+	##print('shape of weight matrix w:', weight.shape)	
 
-	# Build the structures of W and A matrices
+	"""build the structures of W and A matrices"""
 	for n in range(len(sense2idx)):
 		sense_matrix[list(sense2idx.keys())[n]] = matrix_A[n]
 
 	logging.info("Total number of senses %d " %(len(sense_matrix)))
 
-	write_to_file(args.sense_matrix_path, sense_matrix)
+	#write_to_file(args.sense_matrix_path, sense_matrix)
 
 	np.savez(args.save_sense_matrix_path, sense_matrix)
 	logging.info('Written %s' % args.save_sense_matrix_path)
 
-	np.savez(args.save_weight_path, weight)
-	logging.info('Written %s' % args.save_weight_path)
-
+	##np.savez(args.save_weight_path, weight)
+	##logging.info('Written %s' % args.save_weight_path)
