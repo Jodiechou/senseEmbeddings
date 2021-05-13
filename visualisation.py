ab76c10aa62e11d9fdf69f253d852336654f5354
Yi Zhou <33936123+Jodiechou@users.noreply.github.com>
2021-05-13 21:56:01 +0100

    Update visualisation.py

--- visualisation.py
+++ visualisation.py
@@ -55,6 +55,21 @@ def load_glove_embeddings(fn):
 	return embeddings
 
 
+def load_ares_txt(path):
+		sense_vecs = {}
+		with open(path, 'r') as sfile:
+			for idx, line in enumerate(sfile):
+				if idx == 0:
+					continue
+				splitLine = line.split(' ')
+				label = splitLine[0]
+				vec = np.array(splitLine[1:], dtype='float32')
+				dim = vec.shape[0]
+				# print('self.dim', self.dim)
+				sense_vecs[label] = vec
+		return sense_vecs
+
+
 def gelu(x):
     """ Original Implementation of the gelu activation function in Google Bert repo when initialy created.
         For information: OpenAI GPT's gelu is slightly different (and gives slightly different results):
@@ -62,6 +77,10 @@ def gelu(x):
         Also see https://arxiv.org/abs/1606.08415
     """
     return x * 0.5 * (1.0 + torch.erf(x / math.sqrt(2.0)))
+
+
+def relu(x):
+   return np.maximum(0, x)
 
 
 def tsne_plot(sense_embeddings):
@@ -83,7 +102,7 @@ def tsne_plot(sense_embeddings):
 	
 	# print('tokens', tokens)
 	new_values = tsne_model.fit_transform(tokens)
-	print('new_values.shape', new_values.shape)
+	# print('new_values.shape', new_values.shape)
 
 
 	x = []
@@ -98,7 +117,7 @@ def tsne_plot(sense_embeddings):
 	# Label_Com = ['Component 1','Component 2']
 
 	for i in range(len(x)):
-		if i < 8 and i != 1:
+		if i < 7 and i != 1:
 			plt.scatter(x[i], y[i], c='r')
 		else:
 			plt.scatter(x[i], y[i], c='b')
@@ -110,7 +129,8 @@ def tsne_plot(sense_embeddings):
 					 va='bottom')
 	# plt.show()
 
-	path = 'data/figure/bank11_sense_gelu_200.png'
+	# path = 'data/figure/new_bank_sense_linear_useSense_50.png'
+	path = 'data/figure/new_bank_sense_glove_50.png'
 
 	savefig(path, dpi=None, facecolor='w', edgecolor='w',
         orientation='portrait', papertype=None, format=None,
@@ -120,9 +140,10 @@ def tsne_plot(sense_embeddings):
 if __name__ == "__main__":
 
 
-	sv_path = 'data/vectors/senseMatrix.semcor_diagonal_gelu_large_300_200.npz'
+	sv_path = 'data/vectors/senseMatrix.semcor_diagonal_linear_large_bertlast4layers_multiword_300_50.npz'
 	# b_path = 'data/vectors/paramB.semcor_diagonal_gelu_BiCi_noval_1024_300_50.npz'
 	# c_path = 'data/vectors/paramC.semcor_diagonal_gelu_BiCi_noval_1024_300_50.npz'
+	ares_embedding_path = 'external/ares/glove_bert_large.txt'
 
 	glove_embedding_path = 'external/glove/glove.840B.300d.txt'
 
@@ -130,51 +151,62 @@ if __name__ == "__main__":
 	glove_embeddings = load_glove_embeddings(glove_embedding_path)
 	logging.info("Done. Loaded words from GloVe embeddings")
 
-	words = ['money', 'fund', 'cash', 'mortgagee', 'depository', 'lender', 'cay', 'beach', 'coast', 'shore', 'riverside']
+	# words = ['money', 'currency', 'dollar', 'credit', 'expense', 'payment', 'sea', 'beach', 'hill', 'shore', 'mountain']
 	# words = ['lump', 'torch', 'flashlight', 'headlight', 'candle', 'sidelight', 'portable', 'transportable', 'weightless', 'airy']
 	# words = ['level', 'horizontal', 'even', 'smooth', 'consistent', 'plumb', 'apartment', 'house', 'penthouse', 'accommodation']
 	# words = ['circle', 'circlet', 'band', 'round', 'loop', 'circuit', 'phone', 'call', 'dial', 'buzz', 'bell']
-	# words = ['instruct', 'teach', 'coach', 'tutor', 'educate', 'develop', 'procession', 'succession', 'cavalcade', 'caravan', 'convoy', 'railway']
-	# t_word = 'bank'
+	# words = ['instruct', 'teach', 'coach', 'tutor', 'educate', 'develop', 'cavalcade', 'caravan', 'convoy', 'railway']
 	# selected_senses = ['train%2:31:01::', 'train%1:06:00::']
-	selected_senses = ['bank%1:17:00::', 'bank%1:14:00::']
+	# senses = ['instruct%2:32:00::', 'teach%2:32:00::', 'ccoach%2:32:00::', 'tutor%2:32:00::', 'educate%2:41:00::', 'develop%2:31:00::', 
+	# '', '', '', '', 'railway%1:06:01::']
+	# senses = ['organization%1:14:00::', 'college%1:14:00::', 'university%1:14:00::', 'high_school%1:14:00::', 'school%1:14:00::', 'institution%1:14:00::', 
+	# 'sea%1:17:00::', 'beach%1:17:00::', 'hill%1:17:00::', 'shore%1:17:00::', 'mountain%1:17:00::']
+	senses = ['appropriation%1:21:00::', 'currency%1:21:00::', 'savings%1:21:00::', 'expense%1:21:00::', 'payment%1:21:00::', 
+	'coast%1:17:00::', 'beach%1:17:00::', 'island%1:17:00::', 'canyon%1:17:00::', 'riverbank%1:17:00::']
+	selected_senses = ['bank%1:14:00::', 'bank%1:17:00::']
 	
 	# selected_senses = []
 
-	relu = nn.ReLU(inplace=True)
+	# relu = nn.ReLU(inplace=True)
 
 	A = load_senseMatrices_npz(sv_path)
+	# ares_embeddings = load_ares_txt(ares_embedding_path)
+	# print('len of ares_embeddings', len(ares_embeddings))
 	# B = load_senseMatrices_npz(b_path)
 	# C = load_senseMatrices_npz(c_path)
 	sense_embeddings = {}
-	# for sense in A.keys():
 	for sense in selected_senses:
-		# print('sense', sense)
 		curr_lemma = lemmatize(sense)
-		# if curr_lemma not in glove_embeddings:
-			# continue
-		# if curr_lemma == t_word:
 
-		A_matrix = A[sense]
-		# 	# B_matrix = B[sense]
-		# 	# C_matrix = C[sense]
-		vec_g = glove_embeddings[curr_lemma]
-
-		# senseVec = A_matrix * vec_g
-		senseVec = gelu(torch.from_numpy(A_matrix * vec_g))
-		senseVec = senseVec.cpu().detach().numpy()
-		# 	# senseVec = gelu(torch.from_numpy(A_matrix * vec_g))
-		# 	# senseVec = gelu(torch.from_numpy(C_matrix) * gelu(torch.from_numpy(B_matrix) * gelu(torch.from_numpy(A_matrix * vec_g))))
-		# 	# sense_embeddings[sense] = senseVec.cpu().detach().numpy()
 		if sense == 'bank%1:14:00::':
 			label = 'bank_01'
 		else:
 			label = 'bank_02'
-		sense_embeddings[label] = senseVec
-		# 	# selected_senses.append(sense)
 
-	for w in words:
-		sense_embeddings[w] = glove_embeddings[w]
+		# A_matrix = A[sense]
+		vec_g = glove_embeddings[curr_lemma]
+		# senseVec = A_matrix * vec_g
+
+		# senseVec = gelu(A_matrix * vec_g)
+
+		# ares_vec = ares_embeddings[sense]
+		# sense_embeddings[label] = ares_vec
+
+		# cont_vec = np.concatenate((ares_vec, senseVec), axis=0)
+		sense_embeddings[label] = vec_g
+
+	for s in senses:
+		lemma = lemmatize(s)
+		# A_matrix_temp = A[s]
+		vec_g = glove_embeddings[lemma]
+		# vec = A_matrix_temp * vec_g
+		# cont_vec = np.concatenate((ares_embeddings[s], vec), axis=0)
+		sense_embeddings[lemma] = vec_g
+
+		# sense_embeddings[lemma] = ares_embeddings[s]
+		# sense_embeddings[lemma] = cont_vec
+
+
 
 	# print('sense_embeddings', sense_embeddings)
 	# print('selected_senses', selected_senses)
