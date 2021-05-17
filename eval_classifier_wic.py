1cb74a1400dec2f39fcdcd41caa4bc8a0a2c2f40
Yi Zhou <33936123+Jodiechou@users.noreply.github.com>
2021-05-17 10:42:50 +0100

    Update eval_classifier_wic.py

--- eval_classifier_wic.py
+++ eval_classifier_wic.py
@@ -4,7 +4,6 @@ import argparse
 import logging
 from functools import lru_cache
 from collections import defaultdict
-
 import numpy as np
 import torch
 import torch.nn as nn
@@ -13,28 +12,20 @@ from numpy.linalg import norm
 from nltk.corpus import wordnet as wn
 from nltk.stem import WordNetLemmatizer
 from transformers import BertTokenizer, BertModel, BertForMaskedLM
-
 from sklearn.linear_model import LogisticRegression
 import joblib
-
-wn_lemmatizer = WordNetLemmatizer()
-
-import sys  # for parent directory imports
+import sys
 sys.path.insert(1, os.path.join(sys.path[0], '..'))
-
-
-
-
 logging.basicConfig(level=logging.DEBUG,
 					format='%(asctime)s - %(levelname)s - %(message)s',
 					datefmt='%d-%b-%y %H:%M:%S')
+wn_lemmatizer = WordNetLemmatizer()
 
 
 def get_args(
 		emb_dim = 300,
 		diag = False
 			 ):
-
 	parser = argparse.ArgumentParser(description='Evaluation of WiC solution using LMMS for sense comparison.')
 	parser.add_argument('--emb_dim', default=emb_dim, type=int)
 	parser.add_argument('--gloss_embedding_path', default='data/vectors/gloss_embeddings.npz')
@@ -42,12 +33,11 @@ def get_args(
 	parser.add_argument('--ares_embedding_path', default='external/ares/ares_bert_large.txt')
 	parser.add_argument('-eval_set', default='dev', help='Evaluation set', required=False, choices=['train', 'dev', 'test'])
 	parser.add_argument('-sv_path', help='Path to sense vectors', required=False, default='data/vectors/senseMatrix.semcor_diagonal_linear_large_bertlast4layers_multiword_{}_50.npz'.format(emb_dim))
-	# parser.add_argument('-load_weight_path', default='data/vectors/weight.semcor_gloss_diagonal_linear_2048_{}_50.npz'.format(emb_dim))
+	parser.add_argument('-load_weight_path', default='data/vectors/weight.semcor_diagonal_linear_bertlast4layers_multiword_1024_{}_50.npz'.format(emb_dim))
 	parser.add_argument('--glove_embedding_path', default='external/glove/glove.840B.300d.txt')
 	parser.add_argument('-clf_path', help='Path to .pkl LR classifier', required=False, default='data/models/wic_linear_bertlast4layers_50.pkl')
 	parser.add_argument('-device', default='cuda', type=str)
 	args = parser.parse_args()
-
 	return args
 
 
@@ -175,7 +165,6 @@ def get_bert_embedding(sent):
 			token_vecs.append(encoded_vec)
 			merged_vec = np.array(token_vecs, dtype='float32').mean(axis=0) 
 			merged_vec = torch.from_numpy(merged_vec).to(device)
-			# merged_vec = torch.from_numpy(merged_vec.reshape(768, 1)).to(device)
 		sent_tokens_vecs.append((token, merged_vec))
 
 	return sent_tokens_vecs
@@ -203,7 +192,6 @@ def get_sk_type(sensekey):
 
 def load_gloss_embeddings(path):
 	gloss_embeddings = {}
-	# logging.info("Loading Pre-trained Sense Matrices ...")
 	loader = np.load(path, allow_pickle=True)    # gloss_embeddings is loaded a 0d array
 	loader = np.atleast_1d(loader.f.arr_0)       # convert it to a 1d array with 1 element
 	embeddings = loader[0]				 # a dictionary, key is sense id and value is embeddings
@@ -223,7 +211,6 @@ def load_ares_txt(path):
 				label = splitLine[0]
 				vec = np.array(splitLine[1:], dtype='float32')
 				dim = vec.shape[0]
-				# print('self.dim', self.dim)
 				sense_vecs[label] = vec
 		return sense_vecs
 
@@ -244,13 +231,6 @@ class SensesVSM(object):
 			self.load_npz(self.vecs_path)
 		self.load_aux_senses()
 
-		# self.load_B('data/vectors/paramB.semcor_diagonal_gelu_BiCi_noval_1024_{}_50.npz'.format(300))
-		# self.load_C('data/vectors/paramC.semcor_diagonal_gelu_BiCi_noval_1024_{}_50.npz'.format(300))
-
-		'''
-		if normalize:
-			self.normalize()
-		'''
 
 	def load_txt(self, txt_vecs_path):
 		self.vectors = []
@@ -271,7 +251,6 @@ class SensesVSM(object):
 		loader = np.load(path, allow_pickle=True)    # A is loaded a 0d array
 		loader = np.atleast_1d(loader.f.arr_0)       # convert it to a 1d array with 1 element
 		self.A = loader[0]								 # a dictionary, key is sense id and value is sense matrix 
-		# self.labels = list(self.A.keys())
 		self.labels = list(ares_embeddings.keys())
 		# self.labels = list(lmms.keys())
 		self.labels_set = set(self.labels)
@@ -317,25 +296,17 @@ class SensesVSM(object):
 			self.sks_by_pos[self.sk_postags[s]].append(s)
 		self.known_postags = set(self.sks_by_pos.keys())
 
-	'''
-	def normalize(self, norm='l2'):
-		norms = np.linalg.norm(self.vectors, axis=1)
-		self.vectors = (self.vectors.T / norms).T
-	'''
 
 	def get_A(self, label):
 		if label not in self.A.keys():
 			sense_matrix = None
-
 		else: 
 			sense_matrix = self.A[label]
-
 		# return self.A[self.indices[label]]	## For txt file
 		return sense_matrix	## For npz file
 
 
-	def match_senses(self, vec, currVec_g, lemma=None, postag=None, topn=100):
-	# def match_senses(self, vec, lemma=None, postag=None, topn=100):
+	def match_senses(self, currVec_c, currVec_g, lemma=None, postag=None, topn=100):
 		matches = []
 		relevant_sks = []
 		distance = []
@@ -345,16 +316,7 @@ class SensesVSM(object):
 		for sk in self.labels:
 			if (lemma is None) or (self.sk_lemmas[sk] == lemma):
 				if (postag is None) or (self.sk_postags[sk] == postag):
-                	# print('------sk: ', sk)
-		
 					relevant_sks.append(sk)
-
-
-					# cont_vec_c = torch.cat((vec, vec), 0)
-					# sense_vec = torch.from_numpy(ares_embeddings[sk]).to(device)
-					# sim = torch.dot(cont_vec_c, sense_vec) / (cont_vec_c.norm() * sense_vec.norm())
-					# sense_scores.append(sim)
-
 
 					if sk in self.A.keys():
 						A_matrix = torch.from_numpy(self.A[sk]).to(device)
@@ -362,46 +324,33 @@ class SensesVSM(object):
 					else:
 						static_sense_vec = currVec_g
 
-					# static_sense_vec = relu(static_sense_vec)
-
-					cont_vec_c = torch.cat((vec, vec), 0)
-					# context_vec = cont_vec_c
-					# context_vec = torch.cat((cont_vec_c, static_sense_vec), 0)
-					context_vec = torch.cat((cont_vec_c, currVec_g), 0)
-										
+					cont_vec = torch.cat((currVec_c, currVec_c), 0)
+					context_vec = torch.cat((currVec_g, cont_vec), 0)				
 					ares_vec = torch.from_numpy(ares_embeddings[sk]).to(device)
-					sense_vec = torch.cat((ares_vec, static_sense_vec), 0)
-
+					sense_vec = torch.cat((static_sense_vec, ares_vec), 0)
 					sim = torch.dot(context_vec, sense_vec) / (context_vec.norm() * sense_vec.norm())
 					sense_scores.append(sim)
 
-
 		matches = list(zip(relevant_sks, sense_scores))
 		matches = sorted(matches, key=lambda x: x[1], reverse=True)
-		# print('matches', matches)
 		return matches[:topn]
-
-
-
 
 
 if __name__ == '__main__':
 
 	args = get_args()
-
 	if torch.cuda.is_available() is False and args.device == 'cuda':
 		print("Switching to CPU because Jodie doesn't have a GPU !!")
 		args.device = 'cpu'
-
 	device = torch.device(args.device)
 
 	word2id = dict()
 	word2sense = dict()
-
 	lemmas = []
 	vectors = []
 	sense2idx = []
 
+	relu = nn.ReLU(inplace=True)
 	ares_embeddings = load_ares_txt(args.ares_embedding_path)
 	# lmms = load_lmms(args.lmms_embedding_path)
 	senses_vsm = SensesVSM(args.sv_path, normalize=True)
@@ -414,35 +363,32 @@ if __name__ == '__main__':
 
 	results_path = 'data/results/wic.classify.%s.linear50.txt' % args.eval_set
 	
-	# W = load_weight(args.load_weight_path)
-	# W = torch.from_numpy(W).to(device)
+	W = load_weight(args.load_weight_path)
+	W = torch.from_numpy(W).to(device)
 
 	logging.info("Loading Glove Embeddings........")
 	glove_embeddings = load_glove_embeddings(args.glove_embedding_path)
 	logging.info("Done. Loaded words from GloVe embeddings")
-
 	gloss_vecs = load_gloss_embeddings(args.gloss_embedding_path)
 	
-
-
 	logging.info('Processing sentences ...')
 	n_instances, n_correct = 0, 0
 	with open(results_path, 'w') as results_f:  # store results in WiC's format
 		for wic_idx, wic_entry in enumerate(load_wic(args.eval_set, wic_path='external/wic')):
 			word, postag, idx1, idx2, ex1, ex2, gold = wic_entry
-			
+
 			bert_ex1 = get_bert_embedding(ex1)
 			bert_ex2 = get_bert_embedding(ex2)
 
 			# example1
 			ex1_curr_word, ex1_curr_vector = bert_ex1[idx1]
 			ex1_curr_lemma = wn_lemmatize(word, postag)
-			if ex1_curr_lemma not in glove_embeddings:			
+			if ex1_curr_lemma not in glove_embeddings:
 				vec_g1 = torch.randn(args.emb_dim, dtype=torch.float32, device=device, requires_grad=False)
 			else:
 				vec_g1 = torch.from_numpy(glove_embeddings[ex1_curr_lemma]).to(device)
 				ex1_matches = senses_vsm.match_senses(ex1_curr_vector, vec_g1, lemma=ex1_curr_lemma, postag=postag, topn=None)
-		
+				
 			ex1_A_matrix = senses_vsm.get_A(ex1_matches[0][0])
 			if ex1_A_matrix is None:
 				continue
@@ -451,57 +397,44 @@ if __name__ == '__main__':
 				ex1_static_sense_vec = ex1_sense_matric * vec_g1
 
 			ex1_ares_vec = torch.from_numpy(ares_embeddings[ex1_matches[0][0]]).to(device)
-
 			ex1_sense_vec = torch.cat((ex1_ares_vec, ex1_static_sense_vec), 0)
 			ex1_sense_vec = ex1_sense_vec.cpu().detach().numpy()
-
 			ex1_context_vec = torch.cat((ex1_curr_vector, ex1_curr_vector), 0)
 			ex1_context_vec = torch.cat((ex1_context_vec, vec_g1), 0)
 			ex1_context_vec = ex1_context_vec.cpu().detach().numpy()
-
 
 			# example2
 			ex2_curr_word, ex2_curr_vector = bert_ex2[idx2]
 			ex2_curr_lemma = wn_lemmatize(word, postag)
 			if ex2_curr_lemma not in glove_embeddings:
 				vec_g2 = torch.randn(args.emb_dim, dtype=torch.float32, device=device, requires_grad=False)
+
 			else:
 				vec_g2 = torch.from_numpy(glove_embeddings[ex2_curr_lemma]).to(device)
 				ex2_matches = senses_vsm.match_senses(ex2_curr_vector, vec_g2, lemma=ex2_curr_lemma, postag=postag, topn=None)
-				
+			
 			ex2_A_matrix = senses_vsm.get_A(ex2_matches[0][0])
 			if ex2_A_matrix is None:
-				# ex2_static_sense_vec = vec_g2
 				continue
-
 			else:
 				ex2_sense_matric = torch.from_numpy(ex2_A_matrix).to(device)
 				ex2_static_sense_vec = ex2_sense_matric * vec_g2
 
 			ex2_ares_vec = torch.from_numpy(ares_embeddings[ex2_matches[0][0]]).to(device)
-			# ex2_lmms_vec = torch.from_numpy(lmms[ex2_matches[0][0]])
-
 			ex2_sense_vec = torch.cat((ex2_ares_vec, ex2_static_sense_vec), 0)
 			ex2_sense_vec = ex2_sense_vec.cpu().detach().numpy()
-
 			ex2_context_vec = torch.cat((ex2_curr_vector, ex2_curr_vector), 0)
 			ex2_context_vec = torch.cat((ex2_context_vec, vec_g2), 0)
 			ex2_context_vec = ex2_context_vec.cpu().detach().numpy()
-
-			# '''For baseline ***'''
-			# ex1_best = ex1_curr_vector
-			# ex2_best = ex2_curr_vector
-			# '''***'''
-
 			n_instances += 1
-			
 			s1_sim = np.dot(ex1_context_vec, ex2_context_vec)
 			s2_sim = np.dot(ex1_sense_vec, ex2_sense_vec)
 			s3_sim = np.dot(ex1_context_vec, ex1_sense_vec)
 			s4_sim = np.dot(ex2_context_vec, ex2_sense_vec)
-
-			pred = clf.predict([[s1_sim, s2_sim, s3_sim, s4_sim]])[0]
-
+			s5_sim = np.dot(ex1_context_vec, ex2_sense_vec)
+			s6_sim = np.dot(ex2_context_vec, ex1_sense_vec)
+			pred = clf.predict([[s1_sim, s2_sim, s3_sim, s4_sim, s5_sim, s6_sim]])[0]
+			
 			if pred == True:
 				results_f.write('T\n')
 			else:
@@ -509,7 +442,6 @@ if __name__ == '__main__':
 
 			if pred == gold:
 				n_correct += 1
-
 			acc = n_correct/n_instances
 			logging.info('ACC: %f (%d/%d)' % (acc, n_correct, n_instances))
 
